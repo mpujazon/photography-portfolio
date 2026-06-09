d9cda5388ef2a0edc4b849ae26d23073cd9a94d3
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-09 22:42:50 +0200

    PHO-30.React Router configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css'
+import {Route, Routes} from "react-router";
 
 function App() {
   return (
-      <h1>LensByMike</h1>
+      <Routes>
+        <Route path="/" element={<h1>LensByMike</h1>}/>
+        <Route path="/albums" element={<h1>albums</h1>}/>
+        <Route path="/albums/:slug" element={<h1>album view</h1>}/>
+        <Route path="/photos/:slug" element={<h1>photos</h1>}/>
+        <Route path="/about" element={<h1>about</h1>}/>
+        <Route path="/contact" element={<h1>contact</h1>}/>
+      </Routes>
   )
 }
 
