7e6bfe259bb41af2399b5d2d53dd03013da29437
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-18 11:47:27 +0200

    Pictures now are provided from Homepage component, as they'll be used in Hero and FeaturedWork components

--- src/features/home/pages/Homepage.tsx
+++ src/features/home/pages/Homepage.tsx
@@ -1,10 +1,61 @@
+import type { Picture } from "../../../shared/models/Picture.ts";
 import FeaturedWork from "../components/FeaturedWork/FeaturedWork.tsx";
 import {Hero} from "../components/Hero/Hero.tsx";
 
 export function Homepage() {
+    const MOCK_PICTURES: Picture[] = [
+        {
+            id: 5,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750333/wwmd21mnx029mblyzqac.jpg",
+            title: "Aston Martin Vantage ELMS 2026",
+            category: "Motor"
+        },
+        {
+            id: 6,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750327/jbnhgpscoknssr7pzze4.jpg",
+            title: "ELMS 2026",
+            category: "Motor"
+        },
+        {
+            id: 7,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750322/e6ney7qsj0gixcqaz1hl.jpg",
+            title: "ELMS 2026",
+            category: "Motor"
+        },
+        {
+            id: 8,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750319/cbmfyqmlycujwibtwhqd.jpg",
+            title: "ELMS 2026",
+            category: "Motor"
+        },
+        {
+            id: 5,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750333/wwmd21mnx029mblyzqac.jpg",
+            title: "Aston Martin Vantage ELMS 2026",
+            category: "Motor"
+        },
+        {
+            id: 6,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750327/jbnhgpscoknssr7pzze4.jpg",
+            title: "ELMS 2026",
+            category: "Motor"
+        },
+        {
+            id: 7,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750322/e6ney7qsj0gixcqaz1hl.jpg",
+            title: "ELMS 2026",
+            category: "Motor"
+        },
+        {
+            id: 8,
+            url: "https://res.cloudinary.com/urbancore/image/upload/v1778750319/cbmfyqmlycujwibtwhqd.jpg",
+            title: "ELMS 2026",
+            category: "Motor"
+        },
+    ];
     return (
         <>
-            <Hero/>
+            <Hero pictures={MOCK_PICTURES}/>
             <FeaturedWork/>
         </>
     );
---
+++ src/shared/models/Picture.ts
@@ -0,0 +1,6 @@
+export interface Picture {
+    id:       number;
+    url:      string;
+    title:    string;
+    category: string;
+}
