0e2b720bc2771370b12290d771a5cfc333e259e3
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-18 10:39:51 +0200

    FeaturedWork component created, its header has been done

--- src/features/home/pages/Homepage.tsx
+++ src/features/home/pages/Homepage.tsx
@@ -1,9 +1,11 @@
+import FeaturedWork from "../components/FeaturedWork/FeaturedWork.tsx";
 import {Hero} from "../components/Hero/Hero.tsx";
 
 export function Homepage() {
     return (
         <>
             <Hero/>
+            <FeaturedWork/>
         </>
     );
 };
