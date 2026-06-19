ff9f6ef00c1d512a7ed9cec2d4f99a619f8aa854
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-19 19:28:35 +0200

    FeaturedWork has been renamed to FeaturedAlbum

---
+++ src/features/home/components/FeaturedAlbum/FeaturedAlbum.tsx
@@ -0,0 +1,39 @@
+import { Trans, useTranslation } from "react-i18next";
+import style from "./FeaturedAlbum.module.css";
+import TertiaryButton from "../../../../shared/components/buttons/tertiary/TertiaryButton";
+import { useNavigate } from "react-router";
+import Gallery from "../../../../shared/components/Gallery/Gallery";
+import type { PictureDto } from "../../../../shared/models/Picture";
+
+type FeaturedAlbumProps = {
+    pictures: PictureDto[];
+}
+
+function FeaturedAlbum(props: FeaturedAlbumProps) {
+    const { t } = useTranslation("home");
+    const navigate = useNavigate();
+
+    return (
+        <section className={style.featuredAlbumSection}>
+            <div className={style.featuredAlbumShell}>
+                <header className={style.header}>
+                    <div className={style.header__textContainer}>
+                        <span className={style.header__subtitle}>{t("featuredWork.subtitle")}</span>
+                        <h2 className={style.header__title}>
+                            <Trans i18nKey="featuredWork.title" t={t}>
+                                <span className={style.yellow}></span>
+                            </Trans>
+                        </h2>
+                    </div>
+                    <TertiaryButton
+                        label={t("featuredWork.allAlbumsCTA") }
+                        onClick={() => navigate("/albums")}
+                    />
+                </header>
+                <Gallery pictures={props.pictures}/>
+            </div>
+        </section>
+    )
+}
+
+export default FeaturedAlbum;
