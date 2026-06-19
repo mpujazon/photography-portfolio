646091cd6b0676eca74815cd5c9105227c2859a8
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-19 18:57:38 +0200

    ScrollToTop component created and added to main.tsx to reset the scroll when navigating to other route

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router";
+import {ScrollToTop} from "./shared/components/ScrollToTop/ScrollToTop.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
+          <ScrollToTop/>
           <App />
       </BrowserRouter>
   </StrictMode>,
