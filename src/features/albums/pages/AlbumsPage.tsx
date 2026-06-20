a45b8d7d8bb701d0aa6dfac00887afd306e0cc92
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-20 23:51:00 +0200

    AlbumsPage component basic layout styling added

--- src/features/albums/pages/AlbumsPage.tsx
+++ src/features/albums/pages/AlbumsPage.tsx
@@ -1,3 +1,4 @@
+import AlbumsPageHeader from "../components/AlbumsPageHeader/AlbumsPageHeader";
 import { usePublicAlbums } from "../hooks/usePublicAlbums";
 import style from "./AlbumsPage.module.css";
 
@@ -5,18 +6,19 @@ function AlbumsPage() {
     const { data: albums, isPending, isError, error } = usePublicAlbums();
     
     return (
-        <>
+        <section className={style.page}>
+            <AlbumsPageHeader/>
             {
                 albums?.map(album => 
                     <p
                         style={{color:"white"}}
                         key={album.id}
                     >
-                            {album.title}
+                            {album.title} 
                     </p>
                 )
             }
-        </>
+        </section>
     );
 }
 
