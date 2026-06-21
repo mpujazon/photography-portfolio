7d4120007797b886f2cf58a4d9fbd9c43885f551
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-21 15:20:46 +0200

    PHO-38 Album types updated

--- src/shared/models/Album.ts
+++ src/shared/models/Album.ts
@@ -1,12 +1,14 @@
 import { type PhotoDto } from './Photo';
 
 export interface PublicAlbum {
-    id:           number;
-    slug:         string;
-    title:        string;
-    description?: string;
-    coverPhotoUrl?:     string;
-    isFeatured:   boolean;
+    id:             number;
+    slug:           string;
+    title:          string;
+    subtitle?:      string;
+    description?:   string;
+    coverPhotoUrl?: string;
+    isFeatured:     boolean;
+    numberOfPhotos: number; 
 }
 
 export interface PublicAlbumDetail extends PublicAlbum {
