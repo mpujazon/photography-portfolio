ae7eff7bd708528d721b24ea79e7768dbc5cadc4
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-21 15:23:13 +0200

    PHO-38 Implement album list rendering and loading and error handling in AlbumsPage component

--- src/features/albums/pages/AlbumsPage.tsx
+++ src/features/albums/pages/AlbumsPage.tsx
@@ -1,23 +1,33 @@
+import { useTranslation } from "react-i18next";
+import ErrorMessage from "../../../shared/components/ErrorMessage/ErrorMessage";
+import AlbumCard from "../components/AlbumCard/AlbumCard";
+import AlbumCardSkeleton from "../components/AlbumCard/AlbumCardSkeleton";
 import AlbumsPageHeader from "../components/AlbumsPageHeader/AlbumsPageHeader";
 import { usePublicAlbums } from "../hooks/usePublicAlbums";
 import style from "./AlbumsPage.module.css";
 
 function AlbumsPage() {
-    const { data: albums, isPending, isError, error } = usePublicAlbums();
-    
+    const { data: albums, isPending, isError, error, refetch} = usePublicAlbums();
+    const { t } = useTranslation("albums");
+
+    const content = () => {
+        if(isPending) return Array.from({ length: 5 }, (_, i) => <AlbumCardSkeleton key={i} />);
+        if(isError) return <ErrorMessage description={error.message} onRetry={refetch}/>;
+        if (albums.length === 0) return <p className={style.emptyState}>{t('emptyState')}</p>;
+        return albums?.map((album, index) => 
+                        <AlbumCard 
+                            album={album}
+                            index={index + 1}
+                            key={album.id}
+                        />
+                    )
+        }
     return (
         <section className={style.page}>
             <AlbumsPageHeader/>
-            {
-                albums?.map(album => 
-                    <p
-                        style={{color:"white"}}
-                        key={album.id}
-                    >
-                            {album.title} 
-                    </p>
-                )
-            }
+            <div className={style.albumsList}>
+                {content()}
+            </div>
         </section>
     );
 }
